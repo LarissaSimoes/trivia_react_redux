2cc05978adf11741b701e742ecd4c4f0dcbd1f63
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-03-22 15:42:39 -0300

    Requisito 18 finalizado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../components/Button';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div data-testid="ranking-title">
         Ranking
+        <Button
+          id="btn-go-home"
+          label="Voltar para a tela inicial"
+          onClick={ () => history.push('/') }
+        />
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
